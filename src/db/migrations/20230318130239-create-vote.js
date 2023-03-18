88caba7eecab9f2e61c5a69329d5bc82a6bfc462
ShenyiCui <shenyicui@outlook.com>
2023-03-19 00:03:10 +0800

    :sparkles: feat: add create_at and update_at to migration

--- src/db/migrations/20230318130239-create-vote.js
+++ src/db/migrations/20230318130239-create-vote.js
@@ -23,6 +23,14 @@ module.exports = {
         },
         onDelete: 'CASCADE',
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface) {
--- src/db/migrations/20230318131920-create-rejected.js
+++ src/db/migrations/20230318131920-create-rejected.js
@@ -27,6 +27,14 @@ module.exports = {
         },
         onDelete: 'CASCADE',
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface) {
