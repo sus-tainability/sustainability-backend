79cd3697b3a1598239ab9f63f4807d4d2bea4aa7
a2ron-k <aaronkwah@gmail.com>
2023-03-27 01:16:30 +0800

    🧹chore: update seed data

--- src/db/seeders/20230318142123-sample-story.js
+++ src/db/seeders/20230318142123-sample-story.js
@@ -5,8 +5,8 @@ module.exports = {
   up: async queryInterface => {
     return await queryInterface.bulkInsert('stories', [
       {
-        name: 'Story 1',
-        description: 'Story 1 description',
+        name: 'The Fluffiest Endangered Species',
+        description: 'Red pandas are one of the most adorable and unique animals in the world, but they are also in danger of disappearing forever due to habitat loss and poaching.',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
